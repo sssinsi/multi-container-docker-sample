8d9f2110101c25c51b3dce056146b7d76a99b674
sssinsi <tet643773+github@gmail.com>
2017-11-24 15:14:27 +0900

    パス修正 (#5)

--- store-docker-image.sh
+++ store-docker-image.sh
@@ -7,7 +7,7 @@ DOCKER_LOGIN=`eval ${AWS_ECR_GET_LOGIN}`
 eval ${DOCKER_LOGIN}
 
 # build image
-docker build -t php-fpm:latest -f docker/php-fpm/Dockerfile .
+docker build -t php-fpm:latest -f php-fpm.Dockerfile .
 
 # store image
 docker tag php-fpm:latest xxxxxxxxxxxx.dkr.ecr.ap-northeast-1.amazonaws.com/php-fpm:latest
