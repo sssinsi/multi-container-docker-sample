6a66fe58e79e06c6834e44172f843f9ac0a36bee
sssinsi <tet643773+github@gmail.com>
2017-11-24 14:05:42 +0900

    docker imageをecrへ保存するシェルを作成。 (#3)

---
+++ store-docker-image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -xue
+# login docker（プロフィールが必要な場合）
+AWS_ECR_GET_LOGIN="aws ecr get-login --no-include-email --region ap-northeast-1 --profile ${AWSCLI_PROFILE}"
+DOCKER_LOGIN=`eval ${AWS_ECR_GET_LOGIN}`
+eval ${DOCKER_LOGIN}
+
+# build image
+php composer install --no-dev
+docker build -t php-fpm:latest -f docker/php-fpm/Dockerfile .
+
+# store image
+docker tag php-fpm:latest xxxxxxxxxxxx.dkr.ecr.ap-northeast-1.amazonaws.com/php-fpm:latest
+docker push xxxxxxxxxxxx.dkr.ecr.ap-northeast-1.amazonaws.com/php-fpm:latest
